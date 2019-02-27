8759532dc6ebb17d439255766ad3ca8d354c4da9
bluelife <thebulelife@163.com>
2019-02-28 00:13:53 +0800

    ....

--- index.js
+++ index.js
@@ -290,7 +290,7 @@ mongoose.connect('mongodb://localhost:27017/blog', {
         });
     } else {
         // 数据库连接成功后监听80/443端口
-        if (platform.toLowerCase() == 'darwin' || platform.toLowerCase() == 'win32') {
+        // if (platform.toLowerCase() == 'darwin' || platform.toLowerCase() == 'win32') {
             app.listen(80);
             const server = http2.createServer(ssloptions, app);
             server.listen(443);
@@ -303,13 +303,13 @@ mongoose.connect('mongodb://localhost:27017/blog', {
                 title: 'blog.mcloudhub.com',
                 message: 'the server started . http server listener port 80 https server listener port 443 .'
             });
-        } else {
-            app.listen(3002);
-            notifier.notify({
-                title: 'blog.mcloudhub.com',
-                message: 'the server started . http server listener port 3002 .'
-            });
-        }
+        // } else {
+        //     app.listen(3002);
+        //     notifier.notify({
+        //         title: 'blog.mcloudhub.com',
+        //         message: 'the server started . http server listener port 3002 .'
+        //     });
+        // }
     }
 });
 
